06d38e7b6a6ee0dac10b580c9ebeac9476106ba8
Thaifitus <nththai20@clc.fitus.edu.vn>
2022-08-11 08:57:33 +0700

    Delete old files

--- src/sign up/homepage.js
+++
@@ -1,8 +0,0 @@
-var menuIcon = document.querySelector(".header__navbar-icon--sidebar");
-var sidebar = document.querySelector(".grid__column-2");
-var sidebar2 = document.querySelector(".category-item")
-
-menuIcon.onclick=function(){
-    sidebar.classList.toggle("small-sidebar");
-    sidebar2.classList.toggle("small-sidebar")
-}
